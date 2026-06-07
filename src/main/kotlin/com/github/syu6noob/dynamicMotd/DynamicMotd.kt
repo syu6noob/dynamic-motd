6f39f5dfcb85c247c176cb6d6820104012334594
syu6noob <kusahebi@outlook.jp>
2026-06-07 22:24:11 +0900

    initial commit

---
+++ src/main/kotlin/com/github/syu6noob/dynamicMotd/DynamicMotd.kt
@@ -0,0 +1,14 @@
+package com.github.syu6noob.dynamicMotd
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DynamicMotd : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
